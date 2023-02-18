7e5d59c948e6cff2f6b05907dac66edd95d5735e
MarkStega <mark@optimiumhealth.com>
2023-02-18 12:47:50 +0000

    Deploying to gh-pages from @ Material-Blazor/Material.Blazor@e0426a6f02f2e82619295300589648697c2bd15d 🚀

---
+++ _content/Material.Blazor.Website.MD3/js/app.js
@@ -0,0 +1,19 @@
+﻿window.material_blazor_website = {
+    themeSetter: {
+        setTheme: function (sheetName, minify) {
+            let extension = ".css";
+
+            if (minify === true) {
+                extension = ".min.css";
+            }
+
+            document.getElementById("app-style").setAttribute("href", "_content/Material.Blazor.Website/css/" + sheetName + extension);
+        }
+    },
+
+    baseHref: {
+        getBaseURI: function () {
+            return document.getElementsByTagName("base")[0].getAttribute("href");
+        }
+    }
+}
